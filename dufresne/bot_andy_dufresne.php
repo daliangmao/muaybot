f98cd345215f9e40db4c5f3421b7ddabf4c4dc3a
mac's andy <daliangmao@macss-Mac-mini.local>
2017-07-11 18:09:26 +0700

    update

--- dufresne/bot_andy_dufresne.php
+++ dufresne/bot_andy_dufresne.php
@@ -1,10 +1,11 @@
 <?php
+/*
 require __DIR__."/../vendor/autoload.php";
 
 define("LINE_MESSAGING_API_CHANNEL_SECRET", 'ca58afbfc13493e4d2788d09dba681d8');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'mgBXorDy3WeSkzH+z1AbUqx8dckgidMF4zl+0XT1a94cY7gi9vpKMrEoEQa9Vfbx+LQX/+xR9xvc8YRej5PhQ+ViX5op/QSUWItMIC2Xx9xgjpK3fko2kCz2RbwNz49tDu/a3ZL7WeohDzlRzAOXqgdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot(new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN), ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]);
-
+*/
 $access_token = 'mgBXorDy3WeSkzH+z1AbUqx8dckgidMF4zl+0XT1a94cY7gi9vpKMrEoEQa9Vfbx+LQX/+xR9xvc8YRej5PhQ+ViX5op/QSUWItMIC2Xx9xgjpK3fko2kCz2RbwNz49tDu/a3ZL7WeohDzlRzAOXqgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
