99e72b74596cfd6371479d31451e02d6355baed7
mac's andy <daliangmao@macss-Mac-mini.local>
2017-07-18 13:55:33 +0700

    update

--- hook.php
+++ hook.php
@@ -8,6 +8,9 @@ $cfg = ['ticha'=>['token'=>'NOvBacA+SXkjtPsNXBuTp9rY6v9NzGSIDaNUQY9w7ByGTY8Z4+0N
 		'kung'=>['token'=>'q23T4FgquDCxS5KoAGeBPn1mI8Ia2JyDD/p0edrTedjWjRieaDmX/4Zjvtq37BuniVULAxCinSOJuOXh8sI0ak3FbrN8DOlzhMFV/l3koRtfzBTiIm8AGsjXV1p4daOwa8nyWDXuULoCQDc88BXHDwdB04t89/1O/w1cDnyilFU=',
 				'secrete'=>'0fc16138deb58bff59be7bcc224ee936',
 				'id'=>2634],
+		'champ'=>['token'=>'wgKECv8bdrcqC3rvpPjhmgt67l4xqDiSrShPGmwiV3XavhRMfO/NQ0PLhNEvRUpr0MSKUK4B6vUjtIaIV0Cxoph9yyoZzmmvzWoiONwCwj2Y4MVXLTdCePS++uZCrGAD/kV33E0Lmbart4P0bcUniQdB04t89/1O/w1cDnyilFU=',
+				'secrete'=>'b2cb2d1c419fc9a0458774b80d1a6ab6',
+				'id'=>9547],
 		'pos'=>['token'=>'ViclzPrREkIvT0j3LEIi1rbfwj87wEo/wSRjXHc4axmjMTNqUkoe+X5TwgiRVyj44SMPG36wwj9v/aFUOA9Iv6XxO/k7pok6cohXz+gjVdtNmDweeNOxIPMNLJYhQW29p42uhAOgUl5jy6EvUNtTGgdB04t89/1O/w1cDnyilFU=',
 				'secrete'=>'9eeadd54f98a054686a52249f3724ec4',
 				'id'=>8417]];
