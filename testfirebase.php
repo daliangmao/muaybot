02c16df8a65294a0529cbad7addf683b49f2878a
mac's andy <daliangmao@macss-Mac-mini.local>
2017-07-20 12:44:44 +0700

    update

--- testfirebase.php
+++ testfirebase.php
@@ -1,10 +1,10 @@
 <?php
-require __DIR__."/vendor/autoload.php";
+include dirname(__DIR__) . '/vendor/autoload.php';
 
 use Firebase\Firebase;
+/*
 use Firebase\Auth\TokenGenerator;
 
-/*
 $tokenGenerator = new TokenGenerator("HcvQ1x64BFvvoKwHOeVAiX0yEnjXgdhiwdxUDDu9");
 
 $token = $tokenGenerator->generateToken(['email' => 'muayhooapp@gmail.com'])
